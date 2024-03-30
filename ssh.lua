4e4a767137c3b43cf5866b4abd7117d876710ffa
luigi <virtual.employee@gmail.com>
2024-03-29 21:38:59 -0600

    Made come changes to config and colors.

--- ssh.lua
+++ ssh.lua
@@ -4,8 +4,13 @@ return {
 		remote_address = '192.168.2.15',
 		username = 'luigi',
 	},
-}
+	{
+		name = 'Dev3-53',
+		remote_address = '192.168.2.53',
+		username = 'luigi',
+	}
 
+}
 
 
 --return{
--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,21 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local config = wezterm.config_builder()
---local ssh_domains = require('ssh')
 local tab = require 'tab'
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 config.color_scheme = 'Gotham (terminal.sexy)'
 config.font_dirs = { 'fonts' }
-config.font = wezterm.font("CaskaydiaCove Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font = wezterm.font("CaskaydiaCove Nerd Font" )
 config.font_size = 15.5
---config.ssh_domains = ssh_domains
+config.ssh_domains = require 'ssh'
+config.text_background_opacity = 0.3
+
+--config.term = 'wezterm'
 
 tab.apply_to_config(config)
 return config
